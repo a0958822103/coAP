832a8c8611be1e1632f746a796fd699ceab11508
a0958822103 <a0958822103@gmail.com>
2026-02-02 15:55:47 +0800

    correct password

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 // --- 網路設定 ---
 const char* ssid     = "test_AP";
-const char* password = "passwordis1234";
+const char* password = "passwordis123";
 
 // --- Raspberry Pi 設定 ---
 const char* server_ip = "192.168.4.10"; 
